a9029abc8bfa7d5f6ee71679634ca21bfa9b846b
Khai Nguyen <khai.b.nguyen@gmail.com>
2015-04-16 00:42:13 -0700

    Updated resume

--- js/main.js
+++ js/main.js
@@ -46,35 +46,3 @@ var setHeader = function() {
 };
 
 $(document).ready(setHeader);
-
-/*
-'<header>\
-        <div class="row" style="padding-top: 10px">\
-          <div class="col-xs-12 col-md-10 col-md-push-1">\
-            <div class="test" id="site-name">\
-              <h1>Site Name</h1>\
-              <p style="font-size: 18px; font-style: italic">Site Description</p>\
-            </div>\
-          </div>\
-        </div>\
-        <!--Name-->\
-        <div class="row">\
-          <!--Navigation-->\
-          <div class="col-md-6 col-md-offset-3">\
-              <div class="row" >\
-                <div id="navbar">\
-                  <a href="index.html"><div class="col-xs-12 col-sm-4 col-md-4">\
-                    PROJECTS\
-                  </div></a>\
-                  <a href="about.html"><div class="col-xs-12 col-sm-4 col-md-4">\
-                    ABOUT\
-                  </div></a>\
-                  <a href="other.html"><div class="col-xs-12 col-sm-4 col-md-4">\
-                    OTHER\
-                  </div></a>\
-                </div>\
-             </div>\
-          </div>\
-        </div>\
-      </header>'
-*/
